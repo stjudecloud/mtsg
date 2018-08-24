4428ed64bdce58c52d97ba94077258638733abec
Michael Macias <michael.macias@stjude.org>
2018-08-24 13:29:39 -0500

    sample_sheet: Add tests

--- src/sample_sheet.rs
+++ src/sample_sheet.rs
@@ -7,49 +7,69 @@ use regex::Regex;
 
 static DEFAULT_TAG: &str = "unknown";
 
+struct NameTagPair {
+    name: String,
+    tag: String,
+}
+
 pub fn generate<P, Q>(src: P, dst: Q) -> io::Result<()>
 where
     P: AsRef<Path>,
     Q: AsRef<Path>,
 {
-    let file = File::create(dst)?;
-    let mut writer = BufWriter::new(file);
-
     let mut pattern = src.as_ref().to_path_buf();
     pattern.push("*.vcf");
 
-    let pattern = pattern.to_str().ok_or_else(|| {
-        io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("invalid path: {:?}", pattern),
-        )
+    let pattern = format!("{}", pattern.display());
+
+    let pathnames = list_directory(&pattern).map_err(|e| {
+        io::Error::new(io::ErrorKind::Other, e)
     })?;
 
-    let pathnames = glob(pattern)
-        .map_err(|e| {
-            io::Error::new(io::ErrorKind::InvalidInput, format!("{}", e))
-        })?
-        .filter_map(Result::ok);
+    let pairs = build_pairs(&pathnames);
 
-    for pathname in pathnames {
-        let sample_id = pathname.file_stem()
-            .and_then(|n| n.to_str())
-            .ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    format!("invalid path: {:?}", pattern),
-                )
-            })?;
+    let file = File::create(dst)?;
+    let mut writer = BufWriter::new(file);
 
-        let disease = parse_disease(sample_id).unwrap_or_else(|| {
-            warn!("could not extract disease from sample name '{}'", sample_id);
-            DEFAULT_TAG
-        });
+    write_table(&mut writer, &pairs)
+}
 
-        writeln!(&mut writer, "{}\t{}", sample_id, disease)?;
+fn write_table<W>(writer: &mut W, pairs: &[NameTagPair]) -> io::Result<()> where W: Write {
+    for pair in pairs {
+        writeln!(writer, "{}\t{}", pair.name, pair.tag)?;
     }
 
     Ok(())
+}
+
+/// Returns a list of the basenames given a search pattern.
+///
+/// This silently discards invalid and missing filenames.
+fn list_directory(pattern: &str) -> Result<Vec<String>, String> {
+    let basenames = glob(pattern)
+        .map_err(|e| format!("{}", e))?
+        .filter_map(Result::ok)
+        .filter_map(|path| basename(&path))
+        .collect();
+
+    Ok(basenames)
+}
+
+fn basename<P>(path: P) -> Option<String> where P: AsRef<Path> {
+    path.as_ref().file_stem().and_then(|n| n.to_str()).map(String::from)
+}
+
+fn build_pairs(names: &[String]) -> Vec<NameTagPair> {
+    names.iter()
+        .map(|name| {
+            let tag = parse_disease(name).unwrap_or_else(|| {
+                warn!("could not extract disease from sample name '{}'", name);
+                DEFAULT_TAG
+            });
+
+            NameTagPair { name: name.clone(), tag: tag.to_string() }
+        })
+        .collect()
 }
 
 /// Extracts the disease name from a sample ID.
@@ -69,7 +89,51 @@ fn parse_disease(name: &str) -> Option<&str> {
 
 #[cfg(test)]
 mod tests {
-    use super::parse_disease;
+    use super::*;
+
+    #[test]
+    fn test_write_table() {
+        let pairs = vec![
+            NameTagPair { name: String::from("SJACT001_D"), tag: String::from("ACT") },
+            NameTagPair { name: String::from("SJBALL020013_D1"), tag: String::from("BALL") },
+        ];
+
+        let mut data = Vec::new();
+
+        write_table(&mut data, &pairs).unwrap();
+
+        let actual = String::from_utf8(data).unwrap();
+
+        let expected = "\
+SJACT001_D\tACT
+SJBALL020013_D1\tBALL
+";
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_basename() {
+        assert_eq!(basename("sample.vcf"), Some(String::from("sample")));
+        assert_eq!(basename("sample.vcf.gz"), Some(String::from("sample.vcf")));
+        assert_eq!(basename("sample"), Some(String::from("sample")));
+        assert_eq!(basename(".vcf"), Some(String::from(".vcf")));
+        assert_eq!(basename(""), None);
+    }
+
+    #[test]
+    fn test_build_pairs() {
+        let names = vec![String::from("SJACT001_D"), String::from("SJBALL020013_D1")];
+        let pairs = build_pairs(&names);
+
+        assert_eq!(pairs.len(), 2);
+
+        assert_eq!(pairs[0].name, "SJACT001_D");
+        assert_eq!(pairs[0].tag, "ACT");
+
+        assert_eq!(pairs[1].name, "SJBALL020013_D1");
+        assert_eq!(pairs[1].tag, "BALL");
+    }
 
     #[test]
     fn test_parse_disease() {
