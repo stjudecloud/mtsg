4100166f2e77d68494f21a9790e5af363db402a5
Michael Macias <michael.macias@stjude.org>
2020-09-22 11:12:33 -0500

    stubs: Add type definition for spss.single_sample

---
+++ stubs/sigproSS/spss.pyi
@@ -0,0 +1,9 @@
+def single_sample(
+    data: str,
+    output: str,
+    ref: str = "GRCh37",
+    sig_database: str = "sig_database",
+    check_rules: bool = True,
+    exome: bool = False,
+) -> None:
+    ...
