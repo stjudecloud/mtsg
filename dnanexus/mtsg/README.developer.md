d6097aa058a8ad323f550f6634df8788ad92f87a
Michael Macias <michael.macias@stjude.org>
2020-09-25 15:10:49 -0500

    dnanexus: Update app

--- dnanexus/mtsg/README.developer.md
+++ dnanexus/mtsg/README.developer.md
@@ -1,7 +1,7 @@
 # St. Jude Mutational Signatures (dev)
 
 The main script runs multiple containers with a pre-built Mutational Signatures
-image. See `README.md` for a description of the process.
+image.
 
 ## Build
 
@@ -11,20 +11,6 @@ $ mkdir resources/tmp
 $ docker save mtsg | gzip > resources/tmp/mtsg-latest.tar.gz
 $ dx build
 ```
-
-## Test
-
-The included tests are a suite of system tests that run the main entry point
-with various input arguments. The suite is run locally, preparing input data
-manually and shimming dxpy commands.
-
-Tests can be run using [shunit2].
-
-```
-$ test/run
-```
-
-[shunit2]: https://github.com/kward/shunit2
 
 ## Versioning
 
--- dnanexus/mtsg/src/mtsg.sh
+++ dnanexus/mtsg/src/mtsg.sh
@@ -3,90 +3,34 @@
 main() {
     set -ex
 
-    DEFAULT_PREFIX=mtsg
-    MTSG_SPLIT_VCF_EXTRA_ARGS=""
-
-    DATA_DIR=$HOME/data
-    RESULTS_DIR=$HOME/results
+    DATA_PREFIX=$HOME/data
+    RESULTS_PREFIX=$HOME/results
 
     gzip --decompress --stdout $RESOURCES/tmp/mtsg-latest.tar.gz | docker load
 
-    mkdir -p $DATA_DIR $RESULTS_DIR/vcfs
+    mkdir -p $DATA_PREFIX $RESULTS_PREFIX
 
-    dx-download-all-inputs --parallel --except sample_sheet
-    mv $HOME/in/vcfs/**/* $DATA_DIR
-
-    if [[ -z "$prefix"  ]]; then
-        if [[ ${#vcfs[@]} -eq 1 ]]; then
-            VCF=$(dx describe --name "${vcfs[0]}")
-            PREFIX=$(basename "$(basename "$VCF" .vcf)" .vcf.gz)
-        else
-            PREFIX=$DEFAULT_PREFIX
-        fi
-    else
-        PREFIX=$prefix
-    fi
-
-    if [[ ! -z "$disable_column" ]]; then
-        MTSG_SPLIT_VCF_EXTRA_ARGS="--disable-column $disable_column"
-    fi
-
-    SAMPLE_SHEET="$PREFIX.sample.sheet.txt"
-    SIGNATURES_HTML="$PREFIX.signatures.html"
-    SIGNATURES_TXT="$PREFIX.signatures.txt"
+    dx-download-all-inputs --parallel
+    mv $HOME/in/vcf_srcs/**/* $DATA_PREFIX
 
     docker run \
-        --mount type=bind,source=$DATA_DIR,target=/data,readonly \
-        --mount type=bind,source=$RESULTS_DIR,target=/results \
-        --entrypoint /bin/bash \
+        --mount type=bind,source=$DATA_PREFIX,target=/data \
+        --mount type=bind,source=$RESULTS_PREFIX,target=/results \
         mtsg \
-        -c \
-        "/opt/mtsg/bin/mtsg \
-        --verbose \
-        split-vcf \
-        --output-directory /results/vcfs \
-        $MTSG_SPLIT_VCF_EXTRA_ARGS \
-        /data/*.vcf*"
-
-    if [[ -z "$sample_sheet" ]]; then
-        docker run \
-            --mount type=bind,source=$RESULTS_DIR,target=/results \
-            mtsg \
-            --verbose \
-            generate-sample-sheet \
-            --output "/results/$SAMPLE_SHEET" \
-            /results/vcfs
-    else
-        dx download --output "$RESULTS_DIR/$SAMPLE_SHEET" "$sample_sheet"
-    fi
-
-    sample_sheet_out=$(dx upload --brief "$RESULTS_DIR/$SAMPLE_SHEET")
-
-    docker run \
-        --mount type=bind,source=$RESULTS_DIR,target=/results \
-        mtsg \
-        --verbose \
         run \
-        --output-directory /results \
-        --prefix "$PREFIX" \
-        --genome-build $genome_build \
-        --min-burden $min_burden \
-        --min-contribution $min_contribution \
-        /results/vcfs \
-        "/results/$SAMPLE_SHEET"
+        --dst-prefix /results \
+        /data
 
     docker run \
-        --mount type=bind,source=$RESULTS_DIR,target=/results \
+        --mount type=bind,source=$RESULTS_PREFIX,target=/results \
         mtsg \
-        --verbose \
         visualize \
-        --output "/results/$SIGNATURES_HTML" \
-        "results/$SIGNATURES_TXT"
+        --output /results/Sig_activities.txt \
+        /results/Sig_activities.html
 
-    signatures_txt=$(dx upload --brief "$RESULTS_DIR/$SIGNATURES_TXT")
-    signatures_html=$(dx upload --brief "$RESULTS_DIR/$SIGNATURES_HTML")
+    signature_activities_txt=$(dx upload --brief $RESULTS_PREFIX/Sig_activities.txt)
+    signature_activities_html=$(dx upload --brief $RESULTS_PREFIX/Sig_activities.html)
 
-    dx-jobutil-add-output --class file signatures_txt "$signatures_txt"
-    dx-jobutil-add-output --class file signatures_html "$signatures_html"
-    dx-jobutil-add-output --class file sample_sheet_out "$sample_sheet_out"
+    dx-jobutil-add-output --class file signature_activities_txt "$signature_activities_txt"
+    dx-jobutil-add-output --class file signature_activities_html "$signature_activities_html"
 }
