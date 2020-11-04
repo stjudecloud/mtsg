4b7a45ac770b1391241a1082b1ab2048786d7f6a
Michael Macias <michael.macias@stjude.org>
2020-11-04 15:30:35 -0600

    scripts/prepare_reference: Fix merging of sparse matrices

--- scripts/prepare_reference.py
+++ scripts/prepare_reference.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 HEADER_DELIMITER = "|"
 
@@ -16,6 +16,15 @@ class Disease:
     def __init__(self, name: str, code: str) -> None:
         self.name = name
         self.code = code
+
+
+class Sample:
+    name: str
+    contributions: Dict[str, int]
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.contributions = {}
 
 
 def normalize_sample_name(s: str) -> str:
@@ -44,50 +53,75 @@ def read_sample_info(src: Path) -> Dict[str, Disease]:
 
 sample_name_diseases = read_sample_info(sample_info_src)
 
-prepared_headers = ["Samples"]
-readers = []
+signatures = set()
+samples: Dict[str, Sample] = {}
 
 for activities_src in activities_srcs:
     f = activities_src.open(newline="")
     reader = csv.reader(f, delimiter="\t")
     headers = next(reader)
 
-    raw_sample_names = headers[1:]
+    sample_names = headers[1:]
 
-    for raw_sample_name in raw_sample_names:
-        sample_name = normalize_sample_name(raw_sample_name)
+    for row in reader:
+        signature = row[0]
+        contributions = row[1:]
 
-        if sample_name in sample_name_diseases:
-            disease = sample_name_diseases[sample_name]
+        signatures.add(signature)
 
-            header = "{}{}{}{}{}".format(
-                sample_name,
-                HEADER_DELIMITER,
-                disease.code,
-                HEADER_DELIMITER,
-                disease.name,
-            )
+        for (sample_name, contribution) in zip(sample_names, contributions):
+            if sample_name in samples:
+                sample = samples[sample_name]
+            else:
+                sample = Sample(sample_name)
+                samples[sample_name] = sample
 
-            prepared_headers.append(header)
-        else:
-            print(
-                "WARN: unknown sample name '{}'".format(sample_name),
-                file=sys.stderr,
-            )
-            prepared_headers.append(sample_name)
+            sample.contributions[signature] = int(contribution)
 
-    readers.append(reader)
+sample_names = list(samples.keys())
+sample_names.sort()
+
+activated_signatures = list(signatures)
+activated_signatures.sort()
 
 writer = csv.writer(sys.stdout, delimiter="\t", lineterminator="\n")
-writer.writerow(prepared_headers)
 
-for rdrs in zip(*readers):
-    row_out = []
+prepared_headers = []
 
-    for i, row_in in enumerate(rdrs):
-        if i == 0:
-            row_out.extend(row_in)
+for raw_sample_name in sample_names:
+    sample_name = normalize_sample_name(raw_sample_name)
+
+    if sample_name in sample_name_diseases:
+        disease = sample_name_diseases[sample_name]
+
+        header = "{}{}{}{}{}".format(
+            sample_name,
+            HEADER_DELIMITER,
+            disease.code,
+            HEADER_DELIMITER,
+            disease.name,
+        )
+
+        prepared_headers.append(header)
+    else:
+        print(
+            "WARN: unknown sample name '{}'".format(sample_name),
+            file=sys.stderr,
+        )
+        prepared_headers.append(sample_name)
+
+writer.writerow(["Samples"] + prepared_headers)
+
+for signature in activated_signatures:
+    row = [signature]
+
+    for sample_name in sample_names:
+        sample = samples[sample_name]
+
+        if signature in sample.contributions:
+            contribution = sample.contributions[signature]
+            row.append(contribution)
         else:
-            row_out.extend(row_in[1:])
+            row.append(0)
 
-    writer.writerow(row_out)
+    writer.writerow(row)
